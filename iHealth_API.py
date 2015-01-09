8ea6974f3b53dcabddba38bbf26ef1cb5d4cef3d
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2015-01-09 23:01:59 +0500

    Added other routes to client

--- iHealth_API.py
+++ iHealth_API.py
@@ -23,13 +23,43 @@ def callback():
     return r
 
 @app.route('/api_bp')
-def call_api_bp():
+def api_bp():
     r = api.get_blood_pressure()
     return r
 
 @app.route('/api_weight')
-def call_api_weight():
+def api_weight():
     r = api.get_weight()
+    return r
+
+@app.route('/api_bg')
+def api_bg():
+    r = api.get_bg()
+    return r
+
+@app.route('/api_blood_oxygen')
+def api_blood_oxygen():
+    r = api.get_blood_oxygen()
+    return r
+
+@app.route('/api_activity_report')
+def api_activity_report():
+    r = api.get_activity_report()
+    return r
+
+@app.route('/api_sleep_report')
+def api_sleep_report():
+    r = api.get_sleep_report()
+    return r
+
+@app.route('/api_food')
+def api_food():
+    r = api.get_food()
+    return r
+
+@app.route('/api_sport_report')
+def api_sport_report():
+    r = api.get_sport_report()
     return r
 
 if __name__ == '__main__':
